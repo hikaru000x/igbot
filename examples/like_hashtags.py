91322dedd12abfe29ca182c158441ba8330c2c7e
hikaru000x <129393392+hikaru000x@users.noreply.github.com>
2023-03-30 21:17:47 +0900

    Update like_hashtags.py

--- examples/like_hashtags.py
+++ examples/like_hashtags.py
@@ -20,7 +20,35 @@ parser.add_argument("hashtags", type=str, nargs="+", help="hashtags")
 args = parser.parse_args()
 
 bot = Bot()
-bot.login(username=args.u, password=args.p, proxy=args.proxy)
+bot.login(username=args.u, password=args.p,
+proxy=args.proxy)
 
-for hashtag in args.hashtags:
-    bot.like_hashtag(hashtag)
+tags = [#競艇予想]
+wait = 25 * 60 # in seconds => 25 minutes
+retry = 5 * 60 * 60 # in hours => 5 hours
+
+while True:
+try:
+for hashtag in tags:
+bot.like_hashtag(hashtag)
+sleep(wait)
+except:
+sleep(retry)
+    
+    def like_medias(self, medias, check_media=True):
+broken_items = []
+if not medias:
+self.logger.info("Nothing to like.")
+return broken_items
+self.logger.info("Start Logging")
+self.logger.info("Going to like %d medias." % (len(medias)))
+start_count = self.total['likes'] 
+for media in tqdm(medias):
+if not self.like(media, check_media):
+self.error_delay()
+broken_items.append(media)
+self.logger.info("Total Like:" + str(self.total['likes']))
+if self.total['likes'] != start_count and self.total['likes'] % 36 == 0: 
+break
+self.logger.info("DONE: Total liked %d medias." % self.total['likes'])
+return broken_items
